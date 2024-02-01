3c15bec6e74f554c3472d0e6e6911a1f1100da11
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2024-02-01 15:42:48 -0300

    Add Header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import logo from "../../../public/images/logo.svg";
+
+export default function Header() {
+	return (
+		<header className="flex justify-center items-center bg-white py-2.5 px-5">
+			<div className="max-w-grid w-full flex justify-between items-center gap-12">
+
+				<Link href="/" className="hover:opacity-80 transition-opacity">
+					<Image width={169} height={42} src={logo} alt="" />
+				</Link>
+
+				<nav className="flex justify-between items-center w-full">
+					<ul className="flex justify-center items-center gap-10	">
+						<li>
+							<a className="text-[#555555] text-base hover:text-[#ED672B] transition-colors">Imobiliárias</a>
+						</li>
+						<li>
+							<a className="text-[#555555] text-base hover:text-[#ED672B] transition-colors">Quero me Associar</a>
+						</li>
+						<li>
+							<a className="text-[#555555] text-base hover:text-[#ED672B] transition-colors">Sobre</a>
+						</li>
+						<li>
+							<a className="text-[#555555] text-base hover:text-[#ED672B] transition-colors">Blog</a>
+						</li>
+					</ul>
+
+					<ul className="flex justify-center items-center gap-8">
+						<li>
+							<a className="text-[#652D91] text-base border border-[#652D91] py-1 px-4 rounded-[60px] hover:text-[#ED672B] hover:border-[#ED672B]  transition-colors">Anunciar Imóvel</a>
+						</li>
+
+						<li>
+							<a className="text-[#555555] text-base hover:text-[#ED672B] transition-colors">Entrar</a>
+						</li>
+					</ul>
+
+				</nav>
+
+			</div>
+
+
+
+
+		</header>
+	)
+}
